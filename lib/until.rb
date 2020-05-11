efb9b37ad8f80f0dbf7e5adf93512b9f510ea0ca
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-11 02:36:00 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,8 +2,10 @@ def using_until
   #made the start amount at 6
   levitation_force = 6
   #until it gets to 10 it will add one more Wingardium Leviosa
+  #to make it return the correct amount it has to be double equals
   until levitation_force == 10
     puts "Wingardium Leviosa"
+    #this adds until it get to ten
   levitation_force += 1
   end
 end
